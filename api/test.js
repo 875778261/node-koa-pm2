dab134544db6078c23d88750cfbacb5b3f10ef20
875778261 <875772861@qq.com>
2020-10-27 22:33:25 +0800

    'publish'

---
+++ api/test.js
@@ -0,0 +1,21 @@
+const { get } = require('http');
+const Router = require('koa-router');
+const router = new Router();
+router
+    .get('/api/', (ctx, next) => {
+        ctx.body = 'holloWorld';
+    })
+    .get('/api/test', (ctx, next) => {
+        ctx.body = {
+            msg: '这是/test',
+            query: ctx.query,
+            queryStr: ctx.querystring
+        }
+    })
+    .post('/apo/users',(ctx,next)=>{
+        ctx.body='这里是/users'
+    })
+    .all('/api/users/:id',(ctx,next)=>{
+        //...
+    });
+    module.exports = router
